b2d22d1047c694959244f8395d4d438ddf7490c7
Motoshi Suzuki <101000230+0muji4@users.noreply.github.com>
2026-01-03 11:31:14 +0900

    Add Android BFF API client models

---
+++ apps/android/src/main/java/com/quranproject/android/network/ApiClient.kt
@@ -0,0 +1,20 @@
+package com.quranproject.android.network
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private val gson = GsonBuilder().create()
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(ApiConfig.baseUrl)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    val service: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
